948a6227d5f9f6fad214229fba3fca3f0dcd3fd2
Andrew Bui <bandrewi98@gmail.com>
2021-11-29 15:14:59 -0800

    problem 4 solved

--- try-out-callbacks-starter/problems/04-my-filter.js
+++ try-out-callbacks-starter/problems/04-my-filter.js
@@ -19,8 +19,14 @@ let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
 console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
-let myFilter = function() {
-
+let myFilter = function (arr, cb) {
+    let res = [];
+    arr.forEach(function (el, i) {
+        if (cb(el)) {
+            res.push(el);
+        }
+    })
+    return res;
 };
 
 
